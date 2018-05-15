6022010494503d5a420513f43caf081cad1d1cd5
navneedh <navneedh@gmail.com>
2018-05-14 17:14:57 -0700

    minor edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,12 @@
 function createImpulse() {
 	var path = document.createElement("path");
-	path.setAttribute("class", "key-anim1");
+	path.setAttribute("class", "tester");
 	path.setAttribute("fill", "none");
 	path.setAttribute("stroke-width","2px");
-	path.setAttribute("stroke","rgba(0,0,0,0.5)")
+	path.setAttribute("stroke","rgba(200,10,10,0.5)")
 	path.setAttribute("d","M40 40, 50 46")
-	path.setAttribute("style", "animation: Drawpath 2.5s linear forwards, animation-delay: 3s, animation-iteration-count: 3;")
 
 var element = document.getElementsByClassName("brain");
 element[0].appendChild(path);
 console.log(path)
 }
-
-createImpulse()
