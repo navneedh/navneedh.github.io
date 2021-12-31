ddea5b435105cda9cb3361069907ee66ca191b51
Navneedh Maudgalya <navneedhm@berkeley.edu>
2021-12-30 18:09:34 -0800

    added jquery

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 function hideLoader() {
     $('#loading').hide();
+    $('#section1').toggle();
 }
 
 // $(window).ready(hideLoader);
 
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
 
-$('.main').hide();
+$('#section1').hide();
 setTimeout(hideLoader, 5 * 1000);
-$('.main').toggle();
